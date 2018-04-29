25039cc1b72752b3e95df98f202ec79d6516d101
m-lab <immatrixchan@gmail.com>
2018-04-29 23:35:48 +0800

    2018-04-29

---
+++ catkin_ws/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(beginner_tutorials_MSG_INCLUDE_DIRS "/home/john/Workspace/ROS/catkin_ws/src/beginner_tutorials/msg")
+set(beginner_tutorials_MSG_DEPENDENCIES std_msgs)
